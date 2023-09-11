2814f54e9a2e28d0dcc0c6bdff6d6e342bc4f54e
Arthur Kalule <kalulearthur@gmail.com>
2023-09-11 05:21:11 +0000

    link between pages

--- app/router.js
+++ app/router.js
@@ -8,4 +8,7 @@ export default class Router extends EmberRouter {
 
 Router.map(function () {
   this.route('about');
+  this.route('contact', {path:'/getting-in-touch'});
+  this.route('contact');
+
 });
